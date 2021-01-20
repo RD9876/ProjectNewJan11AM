b9ce3531bb095c7f43e0bf257be6361ab995c6c0
RD9876 <renudev2706@gmail.com>
2021-01-20 12:31:30 +0530

    added to B

--- src/com/resources/CourseDetails.java
+++ src/com/resources/CourseDetails.java
@@ -3,5 +3,7 @@ package com.resources;
 public class CourseDetails {
 	public static void main(String[] args) {
 		System.out.println("Example");
+		String name = "Greens";
+		System.out.println(name);
 	}
 }
